83abf8d0f78846de6ef640434faa2a0f905c9572
unknown <gadiel.cg@gmail.com>
2017-11-20 13:24:43 -0600

    servicio de login

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -32,64 +31,100 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @SpringBootApplication
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
-  @Autowired
-  private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
-
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
     }
-  }
-  
-	@RequestMapping("/hello")
-	String hello(Map<String, Object> model) {
-    model.put("data", "Esta es la informacion de salida");
-    return "hello";
-	}
 
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
+    @RequestMapping("/")
+    String index() {
+        return "index";
     }
-  }
+
+    @RequestMapping("/db")
+    String db(Map<String, Object> model) {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+            ArrayList<String> output = new ArrayList<String>();
+            while (rs.next()) {
+                output.add("Read from DB: " + rs.getTimestamp("tick"));
+            }
+
+            model.put("records", output);
+            return "db";
+        } catch (Exception e) {
+            model.put("message", e.getMessage());
+            return "error";
+        }
+    }
+
+    @RequestMapping("/hello")
+    String hello(Map<String, Object> model) {
+        model.put("data", "Esta es la informacion de salida");
+        return "hello";
+    }
+
+    @RequestMapping(name="/consulta", method = RequestMethod.POST)
+    String hello(Map<String, Object> model, @RequestParam("u") String user, @RequestParam("p") String password, @RequestParam("t") String tipo) {
+        String query = "select id_centro, superusuario from apac_schema.administrador where usuario = '" + user + "' and contrasenia = '" + password + "';";
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = dataSource.getConnection();
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+
+            Map<String, String> output = new HashMap<String, String>();
+            while (rs.next()) {
+                output.put("id_centro", rs.getString("id_centro"));
+                output.put("superusuario", rs.getString("superusuario"));
+            }
+
+            model.put("usuarios", output);
+            return "consulta";
+        } catch (Exception e) {
+            model.put("message", e.getMessage());
+            return "error";
+        } finally {
+            try {
+                rs.close();
+                stmt.close();
+                connection.close();
+            } catch (Exception e) {
+                model.put("message", e.getMessage());
+                return "error";
+            }
+        }
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
 
 }
