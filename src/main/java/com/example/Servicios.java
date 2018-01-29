daff37cd8809dfe09d85cd9303af7a1d5ac39c97
unknown <gadiel.cg@gmail.com>
2018-01-28 23:34:18 -0600

    se agregan servicios de alumnos, alumnos x centro y evaluadores

--- src/main/java/com/example/Servicios.java
+++ src/main/java/com/example/Servicios.java
@@ -29,30 +29,80 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = Main.API_V1, produces = MediaType.APPLICATION_JSON_VALUE)
 public class Servicios {
 
-    public static final String ENDPOINT = "usuarios";
+    public static final String ENDPOINT_USUARIOS = "usuarios";
+    public static final String ENDPOINT_ALUMNOS = "alumnos";
+    public static final String ENDPOINT_ALUMNOS_X_CENTRO = "alumnosxcentro";
+    public static final String ENDPOINT_EVALUADORES = "evaluadores";
 
     @Autowired
     private DataSource dataSource;
 
-    @RequestMapping(value = ENDPOINT, method = RequestMethod.GET)
+    @RequestMapping(value = ENDPOINT_USUARIOS, method = RequestMethod.GET)
     @ResponseBody
     public HttpEntity<String> getUsers() {
 
-//        ApacBaseDTO dto = new ApacBaseDTO();
-//        dto.setLlave("llave-ñlajsdflñkjaskdljf");
-//        dto.setValor("valor-asdlkfjqowijer");
-//        dto.setDigestion("digestion-alksdf8wro3");
-//        dto.setUltimaActualizacion(System.currentTimeMillis());
-//        
-//        Gson gson = new Gson();
-//        return new ResponseEntity<String>(gson.toJson(dto), HttpStatus.OK);
+        String llave = "heNFkdTzSGbKsseIX4bBB33tZGWFgmyRDlRu2DnCRrRMHxepY0H7liUJmc4I25yFcfThuq8Os59QjUwaQCDAkQ==";
+        String response = consultaBDServicio(llave);
+        
+        if(response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+    }
+
+    @RequestMapping(value = ENDPOINT_ALUMNOS, method = RequestMethod.GET)
+    @ResponseBody
+    public HttpEntity<String> getAlumnos() {
+
+        String llave = "DJGK22ya891zn3I3wFE1KpJacWmiTL+NhKmi7HWgmDkh4C7pmdmFp98vV1ruA+55tdicbvJFQuqJ33IFo1eqyw==";
+        String response = consultaBDServicio(llave);
+        
+        if(response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+    }
+    
+    @RequestMapping(value = ENDPOINT_ALUMNOS_X_CENTRO, method = RequestMethod.GET)
+    @ResponseBody
+    public HttpEntity<String> getAlumnosXCentro() {
+
+        String llave = "P2iExqhMcOepEZa5vUKQrqA62DPUlmhOxQhfPjXLMEJ3KNq+dGMmQm0EaIJZ4xmVZPNVHONmYbmPSxJgxHvaWw==";
+        String response = consultaBDServicio(llave);
+        
+        if(response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+    }
+    
+    @RequestMapping(value = ENDPOINT_EVALUADORES, method = RequestMethod.GET)
+    @ResponseBody
+    public HttpEntity<String> getEvaluadores() {
+
+        String llave = "qzIn2O4fFro4SrquFyiXiRPCes9KqlmNjtFZJzwjgcvXhe6nkfvT+FL81ozGg0YSruFMdhPUjmNDP7DIMbSf5A==";
+        String response = consultaBDServicio(llave);
+        
+        if(response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+    }
+    
+    
+    private String consultaBDServicio(String digestionServicio) {
+
         String query = "select \n"
                 + "	b.llave,\n"
                 + "	b.valor,\n"
                 + "	b.digestion,\n"
                 + "	b.ultima_actualizacion\n"
                 + "	from apac_schema.informacion_base b \n"
-                + "	where b.llave = 'heNFkdTzSGbKsseIX4bBB33tZGWFgmyRDlRu2DnCRrRMHxepY0H7liUJmc4I25yFcfThuq8Os59QjUwaQCDAkQ==';";
+                + "	where b.llave = '" + digestionServicio + "';";
         Connection connection = null;
         Statement stmt = null;
         ResultSet rs = null;
@@ -69,18 +119,17 @@ public class Servicios {
                 dto.setDigestion(rs.getString("digestion"));
                 dto.setUltimaActualizacion(rs.getLong("ultima_actualizacion"));
             }
-            return new ResponseEntity<String>(gson.toJson(dto), HttpStatus.OK);
+            return gson.toJson(dto);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return null;
         } finally {
             try {
                 rs.close();
                 stmt.close();
                 connection.close();
             } catch (Exception e) {
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                return null;
             }
         }
     }
-
 }
